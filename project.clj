dd78ad37a1788e01ad478ed3666d265e2cbc7ded
Alistair Dutton <alistair.dutton@nokia.com>
2013-08-31 23:42:06 +0100

    Ditched snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cower "0.1.0-SNAPSHOT"
+(defproject cower "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [ [org.clojure/clojure "1.5.1"]
