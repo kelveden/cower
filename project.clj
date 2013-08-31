d387ffd671becf35ad0f761b59ef590edab64a9a
Alistair Dutton <alistair.dutton@nokia.com>
2013-08-31 23:27:40 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject cower "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [ [org.clojure/clojure "1.5.1"]
+                  [org.clojure/data.json "0.2.3"]
+                  [com.taoensso/carmine "2.2.0"]
+                  [compojure "1.1.5"] ]
+  :plugins [ [lein-ring "0.8.5"] ]
+  :ring {:handler cower.handler/app}
+  :profiles
+  {:dev {:dependencies [ [ring-mock "0.1.5"] ]}})
---
+++ src/cower/handler.clj
@@ -0,0 +1,56 @@
+(ns cower.handler
+  (:use compojure.core)
+  (:require [clojure.data.json :as json]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [taoensso.carmine :as car :refer (wcar)]))
+
+(def server1-conn { :pool {} :spec {} })
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+
+(defn- key-value-to-map [key-value]
+  (hash-map :name (nth key-value 0) :url (nth key-value 1)))
+
+(defn- key-values-to-json [keys values]
+  (map key-value-to-map (zipmap keys values)))
+
+(defn- search [name-pattern]
+  (let [keys (wcar* (car/keys name-pattern))
+        values (if (empty? keys)
+                 []
+                 (wcar* (apply car/mget keys)))]
+    (key-values-to-json keys values)))
+
+(defn- write-json [thing] 
+  (json/write-str thing :escape-slash false))
+
+(defroutes app-routes
+  (GET "/packages" []
+       (write-json (search "*")))
+  
+  (POST "/packages" [name url]
+        (do 
+          (wcar* (car/set name url))
+          { :status 201 }))
+  
+  (GET "/packages/:name" [name]
+       (let [url (wcar* (car/get name)) ]
+         (if (nil? url)
+           { :status 404 }
+           (write-json
+             (key-value-to-map [name url])))))
+  
+  (DELETE "/packages/:name" [name]
+          (do 
+            (wcar* (car/del name))
+            { :status 204 }))
+  
+  (GET "/packages/search/:name" [name]
+       (write-json
+         (search (str "*" name "*"))))
+  
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
